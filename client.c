50d064d06f1773da263e12e677659fac9d9dc180
Melike Esen <esenmelike48@gmail.com>
2025-10-01 16:36:41 +0300

    Update client.c

--- client.c
+++ client.c
@@ -1,64 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meesen <meesen@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/01 16:13:17 by meesen            #+#    #+#             */
+/*   Updated: 2025/10/01 16:34:00 by meesen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
 #include "minitalk.h"
 
-int	ft_atoi(const char *str)
+int ft_atoi(const char *str)
 {
-    int i;
-    int res;
-    int sign;
+    int i = 0;
+    int res = 0;
+    int sign = 1;
 
-    i = 0;
-    res = 0;
-    sign = 1;
-
-    while (str[i] == ' ' || str[i] >= 9 && str[i] <= 13)
+    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
         i++;
-    if(str[i] == '-' || str[i] == '+')
+    if (str[i] == '-' || str[i] == '+')
     {
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		res = res * 10 + (str[i] - '0');
-		i++;
-	}
-	return (res * sign);
+        if (str[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        res = res * 10 + (str[i] - '0');
+        i++;
+    }
+    return (res * sign);
 }
-void	send_bit(pid_t pid, char c)
+
+void send_bit(pid_t pid, char c)
 {
-    int i;
-    
-    i = 7;
-    while ( i >= 0)
+    int i = 7;
+
+    while (i >= 0)
     {
         if ((c >> i) & 1)
-			kill(pid, SIGUSR2);
-		else
-			kill(pid, SIGUSR1);
-		usleep(150);
-		i--;
-	}
- }
+        {
+            if (kill(pid, SIGUSR2) == -1)
+            {
+                perror("Error sending SIGUSR2");
+                exit(1);
+            }
+        }
+        else
+        {
+            if (kill(pid, SIGUSR1) == -1)
+            {
+                perror("Error sending SIGUSR1");
+                exit(1);
+            }
+        }
+        usleep(150);
+        i--;
+    }
+}
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	pid_t	pid;
-	int		i;
+    pid_t pid;
+    int i;
 
-	if (ac != 3)
-	{
-		ft_putstr("Usage: ./client <PID> <message>\n");
-		return (1);
-	}
-	pid = (pid_t)ft_atoi(av[1]); 
-	i = 0;
-	while (av[2][i])
-	{
-		send_bit(pid, av[2][i]);
-		i++;
-	}
-	return (0);
-} 
+    if (ac != 3)
+    {
+        write(1, "Usage: ./client <PID> <message>\n", 32);
+        return (1);
+    }
 
+    pid = (pid_t)ft_atoi(av[1]);
+    if (pid <= 0)
+    {
+        write(1, "Error: invalid PID\n", 19);
+        return (1);
+    }
 
+    if (kill(pid, 0) == -1)
+    {
+        perror("Error: server not running or PID invalid");
+        return (1);
+    }
+
+    i = 0;
+    while (av[2][i])
+    {
+        send_bit(pid, av[2][i]);
+        i++;
+    }
+    return (0);
+}
