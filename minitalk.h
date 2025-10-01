e5af6372ad9e827edb10325a3c58ecb70377ef88
Melike Esen <esenmelike48@gmail.com>
2025-10-01 16:10:07 +0300

    Add files via upload

---
+++ minitalk.h
@@ -0,0 +1,11 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <unistd.h>
+
+void    ft_putchar(char c);
+void    ft_putnbr(int n);
+void    ft_putstr(char *str);
+
+#endif
